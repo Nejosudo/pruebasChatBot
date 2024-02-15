f75938ba5a631192557292a098c97647efec6698
Nejosudo <150551676+Nejosudo@users.noreply.github.com>
2024-02-15 12:10:47 -0500

    funcionalidad de 2 comandos basicos

---
+++ index.js
@@ -0,0 +1,30 @@
+function sendMessage() {
+    var userInput = document.getElementById("userInput").value
+    var chatLog = document.getElementById("chatLog")
+
+    var userMessage = document.createElement("div")
+    userMessage.classList.add("message")
+    userMessage.innerHTML = "<p>" + userInput + "</p>"
+    chatLog.appendChild(userMessage)
+
+    var botMessage = document.createElement("div")
+    botMessage.classList.add("message", "message-bot")
+    if (userInput != "") {
+        if (userInput === "./prueb_text") {
+            botMessage.innerHTML = ("<p> La ejecución del comando para la prueba de texto se ha realizado con exito. El archivo ./index.js se está ejecutando con total normalidad. </p>")
+        }
+        if (userInput === "./cls") {
+            chatLog.innerHTML = ""
+        }
+        else {
+            botMessage.innerHTML = "<p> Aún no tengo respuesta para dicho mensaje, actualmente solo funcionan comandos: (./prueb_text, ./cls)"
+        }
+        chatLog.appendChild(botMessage)
+    }
+    else {
+        document.getElementById("userInput").value = ""
+    }
+
+    document.getElementById("userInput").value = ""
+    chatLog.scrollTop = chatLog.scrollHeight
+}
