578707cdc27644dbdde248b5f411312c66d92368
Nejosudo <150551676+Nejosudo@users.noreply.github.com>
2024-02-16 10:10:47 -0500

    colores y radio

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ function sendMessage() {
     botMessage.classList.add("message", "message-bot")
     if (userInput != "") {
         if (userInput === "./prueb_text") {
-            botMessage.innerHTML = ("<p> La ejecución del comando para la prueba de texto se ha realizado con exito. El archivo ./index.js se está ejecutando con total normalidad. </p>")
+            botMessage.innerHTML = "<p> La ejecución del comando para la prueba de texto se ha realizado con exito. El archivo ./index.js se está ejecutando con total normalidad. </p>"
         }
         if (userInput === "./cls") {
             chatLog.innerHTML = ""
         }
         else {
             botMessage.innerHTML = "<p> Aún no tengo respuesta para dicho mensaje, actualmente solo funcionan comandos: (./prueb_text, ./cls)"
+        }
+        if (userInput == "Hola" || userInput == "ola" || userInput ==  "hola"){
+            botMessage.innerHTML = "<p>Hola! ¿Como puedo ayudarte el día de hoy?</p>"
         }
         chatLog.appendChild(botMessage)
     }
